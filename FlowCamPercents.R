e6708f118cbeaf3e764098440f54348352d1e45a
Stephen Finnis <stephenfinnis7@gmail.com>
2022-08-15 08:53:49 -0700

    create new script to read in % of sample analyzed in FlowCam analysis

---
+++ FlowCamPercents.R
@@ -0,0 +1,12 @@
+# Function to load multiple packages
+ipak = function(pkg){
+  new.pkg = pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# Choose necessary packages
+packages = c("dplyr", "ggplot2", "ggrepel", "ggthemes", "jcolors", "leaflet", "mapr", "mapview", "readxl", "stringr",
+             "tidyr", "tools", "useful", "vegan", "wpa")
+ipak(packages)
