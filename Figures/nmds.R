d7fd458c4825d2cb3c41de986ede022fe8f4fe1e
Stephen Finnis <stephenfinnis7@gmail.com>
2022-08-19 16:03:04 -0700

    fill in with Gulf data

--- Figures/nmds.R
+++ Figures/nmds.R
@@ -3,6 +3,10 @@
 # Set directory
 source("C:/Users/FINNISS/Desktop/AMPcode/DataProcessing/zooplanktonCounts.R")
 
+
+##### MARITIMES 2020 and 2021 DATA
+# Eventually I will come up with a function to make these plots BUT that will have to wait!!
+
 # alter the dataframe so it is in appropriate format for NMDS
 # names_from: The column whose values will be used as column names
 # values_from: The column whose values will be used as cell values
@@ -12,28 +16,18 @@ marSpecies = marMerge %>% pivot_wider(names_from = class, values_from = abund) %
 # Do NMDS but only include species data
 marNMDS = metaMDS(sqrt(marSpecies[,c(8:60)]), distance = "bray", autotransform=FALSE)
 
-# Making base plot NMDS
-plot(marNMDS$points, type="n")
-# Add the sites in
-text(marNMDS, display="sites", labels=row.names(marSpecies), cex=1, pos=3)
-# Get the text for the legend. Round the NMDS stress to 2 values
-stress09 = paste("2D Stress: ", round(marNMDS$stress, digits=2))
-# Add stress text to top right corner of plot
-legend("topright", legend = c(stress09), cex=1, bty="n") #btw=n gets rid of black box
-
-
 # Get NMDS coordinates from plot
 marCoords = as.data.frame(scores(marNMDS, display="sites"))
+# How stressed am I today
+marStress = paste("2D Stress: ", round(marNMDS$stress, digits=2))
 
 # Plot with Bay and Tide Range
 ggplot() + 
   geom_point(data = marCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(marSpecies$facilityName),
                                        pch = as.factor(marSpecies$tideRange)), size = 5)+ # Use pch=21 to get black outline circles
-  # Change legend names
   scale_fill_discrete(name = "Bay")+
   scale_shape_manual(values=c(21, 22, 23), name = "Tide Range")+ 
-
-  annotate("text", x = max(marCoords$NMDS1), y=max(marCoords$NMDS2), label = stress09, size=3.5, hjust=1)+
+  annotate("text", x = max(marCoords$NMDS1), y=max(marCoords$NMDS2), label = marStress, size=3.5, hjust=1)+
   theme_bw()+
   theme(axis.text = element_blank(),
         #axis.title = element_blank(),
@@ -53,20 +47,14 @@ ggplot() +
 # In Windows (vs Mac), the plots in the Viewer are pixelated and just so ugly
 # ggsave("test.png", g1, scale = 1.7)
 
+#########
 
-
-
-##########
-
-
-
-
-# Plot with Bay and Location (South, Mid, North)
+# Plot Maritimes 2020 d 2021 with Bay and Location (South, Mid, North)
 ggplot() + 
   geom_point(data = marCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(marSpecies$facilityName),
                                    pch = as.factor(marSpecies$location)), size = 5)+ # Use pch=21 to get black outline circles
   # Change legend names
-  scale_fill_discrete(name = "Bay")+
+  scale_fill_discrete(name = "Location")+
   scale_shape_manual(values=c(21, 22, 23), name = "Location within bay")+ 
   
   annotate("text", x = max(marCoords$NMDS1), y=max(marCoords$NMDS2), label = stress09, size=3.5, hjust=1)+
@@ -83,4 +71,63 @@ ggplot() +
   # Need to override and show these as a new new shape otherwise it won't show up
   guides(fill = guide_legend(override.aes = list(shape = 21)),
          shape = guide_legend(override.aes = list(fill = "black")))
+
+
+
+
+##### GULF 2020 and 2021 DATA
+
+
+# alter the dataframe so it is in appropriate format for NMDS
+# names_from: The column whose values will be used as column names
+# values_from: The column whose values will be used as cell values
+
+# colnames(gulfMerge)
+
+# Find a more elegant way to do this!!
+gulfSpecies = gulfMerge %>%
+  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
+  group_by(sampleCode, class, facilityName, waterVolume, tideRange, yearStart, location) %>% summarize(abund = sum(abund)) %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
+
+
+# Do NMDS but only include species data
+gulfNMDS = metaMDS(sqrt(gulfSpecies[,c(7:56)]), distance = "bray", autotransform=FALSE)
+
+# Get NMDS coordinates from plot
+gulfCoords = as.data.frame(scores(gulfNMDS, display="sites"))
+# How stressed am I today
+gulfStress = paste("2D Stress: ", round(gulfNMDS$stress, digits=2))
+
+ggplot() + 
+  geom_point(data = gulfCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(gulfSpecies$facilityName),
+                                   pch = as.factor(gulfSpecies$location)), size = 5)+ # Use pch=21 to get black outline circles
+  # Change legend names
+  scale_fill_discrete(name = "Location")+
+  scale_shape_manual(values=c(21, 22, 23, 24, 25), name = "Location within bay")+ 
+  
+  annotate("text", x = max(gulfCoords$NMDS1), y=max(gulfCoords$NMDS2), label = stress09, size=3.5, hjust=1)+
+  theme_bw()+
+  theme(axis.text = element_blank(),
+        #axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        #legend.position = "none",
+        panel.border=element_rect(color="black", size=1), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank(),
+        plot.margin=unit(c(0.1, 0.1, 0.1, 0.1),"cm"))+
+  # Need to override and show these as a new new shape otherwise it won't show up
+  guides(fill = guide_legend(override.aes = list(shape = 21)),
+         shape = guide_legend(override.aes = list(fill = "black")))
+
+
+
+
+
+
+
+
+
 
