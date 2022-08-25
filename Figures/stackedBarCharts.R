6337dee33c60cb22d7b4b82949d6d165b0ff468e
Stephen Finnis <stephenfinnis7@gmail.com>
2022-08-25 12:20:23 -0700

    add ggplot item to function

--- Figures/stackedBarCharts.R
+++ Figures/stackedBarCharts.R
@@ -60,7 +60,7 @@ stackedBarChart = function(bayData){
   
   # Add this these new classes as a column in the original dataframe
   bayPlotDf = bayData %>%
-    left_join(argyleOther %>%
+    left_join(bayOther %>%
                 # Might be a better way, but I don't want to join the ENTIRE dataframe
                 select(classNew, class, countPerClass), by = c("class" = "class")) %>%
     group_by(classNew, sample, tideRange, location) %>%
@@ -69,17 +69,51 @@ stackedBarChart = function(bayData){
     summarise(sumCount = sum(abund))
   
   
+  # Make ggplot
+  # Write the y-axis label outside of ggplot so I don't get (as) lost in brackets
+  yLabel = expression(paste("Abundance (ind", m^{-3}, ")"))
   
+  
+  stackedGGPlot =
+    ggplot(bayPlotDf, aes(x=sample, y=sumCount, fill=classNew)) +
+      geom_bar(stat="identity", color = "black")+
+      facet_grid(cols = vars(location), scales = "free_x", space = "free_x")+
+      scale_x_discrete(name = "Station")+
+      scale_y_continuous(labels = scales::comma, name = yLabel) +
+      scale_fill_brewer(palette = "Accent", name = "Zooplankton Class")+
+      #theme_minimal(base_family = "Roboto Condensed") +
+      theme_bw()+
+      theme(plot.margin = margin(0.5, 0.5, 0.5, 0.5, unit = "cm"),
+            plot.title = element_text(size = 15, face = "bold"),
+            #strip.text.x = element_text(angle = 270, face = "bold"),
+            strip.placement = "outside",
+            #axis.title.x = element_text(margin = margin(t = 0.5, b = 0.5, unit = "cm")),
+            #axis.title.y = element_blank(),
+            axis.text.x = element_blank(),
+            axis.text.y = element_text(size = 11),
+            axis.text = element_text(size = 14),
+            #legend.position = "none",
+            legend.title = element_text(size=13),
+            panel.grid.major.y = element_blank(),
+            panel.spacing = unit(0.5, "cm"),
+            strip.text.x = element_text(size = 15)
+      )
+  
+  
+  # return as list so you get both the ggplot and processed data
+  returnList = list(bayPlotDf, stackedGGPlot)
+  
+  return(returnList)
   
 }
 
+argyleProcess = stackedBarChart(argyle)
+soberProcess = stackedBarChart(sober)
+whiteheadProcess = stackedBarChart(whitehead)
+cHarbourProcess = stackedBarChart(whitehead)
 
 
-
-
-
-
-
+cHarbourProcess[[2]]
 
 
 
@@ -90,31 +124,3 @@ stackedBarChart = function(bayData){
 # Colours from Quentin
 rrColorPalette<- c("#009E73", "#E69F00", "#0072B2", "#CC79A7", "#F0E442","#D55E00", "#56B4E9","#999999")
 show_col(rrColorPalette)
-
-# Write the y-axis label outside of ggplot so I don't get (as) lost in brackets
-yLabel = expression(paste("Abundance (ind", m^{-3}, ")"))
-
-
-ggplot(argylePlot, aes(x=sample, y=sumCount, fill=classNew)) +
-  geom_bar(stat="identity", color = "black")+
-  facet_grid(cols = vars(location), scales = "free_x", space = "free_x")+
-  scale_x_discrete(name = "Station")+
-  scale_y_continuous(labels = scales::comma, name = yLabel) +
-  scale_fill_brewer(palette = "Accent", name = "Zooplankton Class")+
-  #theme_minimal(base_family = "Roboto Condensed") +
-  theme_bw()+
-  theme(plot.margin = margin(0.5, 0.5, 0.5, 0.5, unit = "cm"),
-    plot.title = element_text(size = 15, face = "bold"),
-    #strip.text.x = element_text(angle = 270, face = "bold"),
-    strip.placement = "outside",
-    #axis.title.x = element_text(margin = margin(t = 0.5, b = 0.5, unit = "cm")),
-    #axis.title.y = element_blank(),
-    axis.text.x = element_blank(),
-    axis.text.y = element_text(size = 11),
-    axis.text = element_text(size = 14),
-    #legend.position = "none",
-    legend.title = element_text(size=13),
-    panel.grid.major.y = element_blank(),
-    panel.spacing = unit(0.5, "cm"),
-    strip.text.x = element_text(size = 15)
-  )
