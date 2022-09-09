329da0ed7a2a9af7479f6d55e7f66dbd4e216e46
Stephen Finnis <stephenfinnis7@gmail.com>
2022-09-09 16:49:49 -0700

    make code less ugly

--- Figures/nmds.R
+++ Figures/nmds.R
@@ -1,11 +1,18 @@
-### NMDS TESTS
+################################################################################
+################################################################################
+### NMDS Ordinations for FlowCam data
 
-# Set directory
+
+################################################################################
+## Read in other scripts
+
+# This has all the plankton data with counts for each file
 source("C:/Users/FINNISS/Desktop/AMPcode/DataProcessing/zooplanktonCounts.R")
 
+################################################################################
+## Process the data
 
-##### MARITIMES 2020 and 2021 DATA
-# Eventually I will come up with a function to make these plots BUT that will have to wait!!
+# MARITIMES
 
 # alter the dataframe so it is in appropriate format for NMDS
 # names_from: The column whose values will be used as column names
@@ -20,6 +27,75 @@ marNMDS = metaMDS(sqrt(marSpecies[,c(8:60)]), distance = "bray", autotransform=F
 marCoords = as.data.frame(scores(marNMDS, display="sites"))
 # How stressed am I today
 marStress = paste("2D Stress: ", round(marNMDS$stress, digits=2))
+
+
+# GULF
+
+# Find a more elegant way to do this!!
+gulfSpecies = gulfMerge %>%
+  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
+  group_by(sampleCode, class, facilityName, waterVolume, tideRange, yearStart, location) %>% summarize(abund = sum(abund)) %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
+
+
+# Do NMDS but only include species data
+gulfNMDS = metaMDS(sqrt(gulfSpecies[,c(7:56)]), distance = "bray", autotransform=FALSE)
+
+# Get NMDS coordinates from plot
+gulfCoords = as.data.frame(scores(gulfNMDS, display="sites"))
+# How stressed am I today
+gulfStress = paste("2D Stress: ", round(gulfNMDS$stress, digits=2))
+
+
+
+# NEWFOUNDLAND
+
+
+# Find a more elegant way to do this!!
+nlSpecies = nlMerge %>%
+  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
+  group_by(sample, class, waterVolume, yearStart, myLabel) %>% 
+  summarize(abund = sum(abund)) %>%
+  # Remove a blank entry. I don't know where this came from!
+  subset(class != "") %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
+
+
+# Do NMDS but only include species data
+nlNMDS = metaMDS(sqrt(nlSpecies[,c(5:42)]), distance = "bray", autotransform=FALSE)
+
+# Get NMDS coordinates from plot
+nlCoords = as.data.frame(scores(nlNMDS, display="sites"))
+# How stressed am I today
+nlStress = paste("2D Stress: ", round(nlNMDS$stress, digits=2))
+
+
+# PACIFIC
+
+# Find a more elegant way to do this!!
+pacSpecies = pacMerge %>%
+  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
+  group_by(sample, class, dataset, sumWaterVolume, yearStart, myLabel) %>% summarize(abund = sum(abund)) %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
+
+
+# Do NMDS but only include species data
+pacNMDS = metaMDS(sqrt(pacSpecies[,c(6:50)]), distance = "bray", autotransform=FALSE)
+
+# Get NMDS coordinates from plot
+pacCoords = as.data.frame(scores(pacNMDS, display="sites"))
+# How stressed am I today
+pacStress = paste("2D Stress: ", round(pacNMDS$stress, digits=2))
+
+
+
+
+
+################################################################################
+## Make the plots
 
 # Plot with Bay and Tide Range
 g1 = 
@@ -50,7 +126,7 @@ ggplot() +
 
 #########
 
-# Plot Maritimes 2020 d 2021 with Bay and Location (South, Mid, North)
+# Maritimes
 g2 = 
 ggplot() + 
   geom_point(data = marCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(marSpecies$facilityName),
@@ -77,32 +153,7 @@ ggplot() +
 ggsave("test2.png", g2)
 
 
-##### GULF 2020 and 2021 DATA
-
-
-# alter the dataframe so it is in appropriate format for NMDS
-# names_from: The column whose values will be used as column names
-# values_from: The column whose values will be used as cell values
-
-# colnames(gulfMerge)
-
-# Find a more elegant way to do this!!
-gulfSpecies = gulfMerge %>%
-  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
-  group_by(sampleCode, class, facilityName, waterVolume, tideRange, yearStart, location) %>% summarize(abund = sum(abund)) %>%
-  pivot_wider(names_from = class, values_from = abund) %>%
-  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
-
-
-# Do NMDS but only include species data
-gulfNMDS = metaMDS(sqrt(gulfSpecies[,c(7:56)]), distance = "bray", autotransform=FALSE)
-
-# Get NMDS coordinates from plot
-gulfCoords = as.data.frame(scores(gulfNMDS, display="sites"))
-# How stressed am I today
-gulfStress = paste("2D Stress: ", round(gulfNMDS$stress, digits=2))
-
-g3 = 
+##### GULF ##### 
 ggplot() + 
   geom_point(data = gulfCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(gulfSpecies$facilityName)),
                                    pch = 21, size = 7, alpha = 0.85)+ # Use pch=21 to get black outline circles
@@ -130,31 +181,7 @@ ggplot() +
 
 
 
-##### PACIFIC DATA
-
-
-# alter the dataframe so it is in appropriate format for NMDS
-# names_from: The column whose values will be used as column names
-# values_from: The column whose values will be used as cell values
-
-colnames(pacMerge)
-
-# Find a more elegant way to do this!!
-pacSpecies = pacMerge %>%
-  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
-  group_by(sample, class, dataset, sumWaterVolume, yearStart, myLabel) %>% summarize(abund = sum(abund)) %>%
-  pivot_wider(names_from = class, values_from = abund) %>%
-  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
-
-
-# Do NMDS but only include species data
-pacNMDS = metaMDS(sqrt(pacSpecies[,c(6:50)]), distance = "bray", autotransform=FALSE)
-
-# Get NMDS coordinates from plot
-pacCoords = as.data.frame(scores(pacNMDS, display="sites"))
-# How stressed am I today
-pacStress = paste("2D Stress: ", round(pacNMDS$stress, digits=2))
-
+##### PACIFIC ##### 
 g4 = 
   ggplot() + 
   geom_point(data = pacCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(pacSpecies$dataset)),
@@ -178,35 +205,7 @@ g4 =
   guides(fill = guide_legend(override.aes = list(shape = 21)),
          shape = guide_legend(override.aes = list(fill = "black")))
 
-##### NEWFOUNDLAND DATA 
-
-
-# alter the dataframe so it is in appropriate format for NMDS
-# names_from: The column whose values will be used as column names
-# values_from: The column whose values will be used as cell values
-
-colnames(nlMerge)
-
-
-# Find a more elegant way to do this!!
-nlSpecies = nlMerge %>%
-  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
-  group_by(sample, class, waterVolume, yearStart, myLabel) %>% 
-  summarize(abund = sum(abund)) %>%
-  # Remove a blank entry. I don't know where this came from!
-  subset(class != "") %>%
-  pivot_wider(names_from = class, values_from = abund) %>%
-  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
-
-
-# Do NMDS but only include species data
-nlNMDS = metaMDS(sqrt(nlSpecies[,c(5:42)]), distance = "bray", autotransform=FALSE)
-
-# Get NMDS coordinates from plot
-nlCoords = as.data.frame(scores(nlNMDS, display="sites"))
-# How stressed am I today
-nlStress = paste("2D Stress: ", round(nlNMDS$stress, digits=2))
-
+##### NEWFOUNDLAND #####  
 g5 = 
   ggplot() + 
   geom_point(data = nlCoords, aes(x=NMDS1, y=NMDS2), fill = "blue",
