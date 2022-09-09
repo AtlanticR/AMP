806fce93f4d49c09a2e226c86bc7ebd1c11ae0f1
Stephen Finnis <stephenfinnis7@gmail.com>
2022-09-09 14:44:26 -0700

    add newfoundland and pacific data!

--- Figures/nmds.R
+++ Figures/nmds.R
@@ -130,7 +130,100 @@ ggplot() +
 
 
 
+##### PACIFIC DATA
 
 
+# alter the dataframe so it is in appropriate format for NMDS
+# names_from: The column whose values will be used as column names
+# values_from: The column whose values will be used as cell values
+
+colnames(pacMerge)
+
+# Find a more elegant way to do this!!
+pacSpecies = pacMerge %>%
+  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
+  group_by(sample, class, dataset, sumWaterVolume, yearStart, myLabel) %>% summarize(abund = sum(abund)) %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
+
+
+# Do NMDS but only include species data
+pacNMDS = metaMDS(sqrt(pacSpecies[,c(6:50)]), distance = "bray", autotransform=FALSE)
+
+# Get NMDS coordinates from plot
+pacCoords = as.data.frame(scores(pacNMDS, display="sites"))
+# How stressed am I today
+pacStress = paste("2D Stress: ", round(pacNMDS$stress, digits=2))
+
+g3 = 
+  ggplot() + 
+  geom_point(data = pacCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(pacSpecies$dataset)),
+             pch = 21, size = 7, alpha = 0.85)+ # Use pch=21 to get black outline circles
+  # Change legend names
+  scale_fill_discrete(name = "Location")+
+  scale_shape_manual(values=c(21, 22, 23, 24, 25), name = "Location within bay")+ 
+  
+  annotate("text", x = max(pacCoords$NMDS1), y=max(pacCoords$NMDS2), label = pacStress, size=3.5, hjust=1)+
+  theme_bw()+
+  theme(axis.text = element_blank(),
+        #axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        #legend.position = "none",
+        panel.border=element_rect(color="black", size=1), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank(),
+        plot.margin=unit(c(0.1, 0.1, 0.1, 0.1),"cm"))+
+  # Need to override and show these as a new new shape otherwise it won't show up
+  guides(fill = guide_legend(override.aes = list(shape = 21)),
+         shape = guide_legend(override.aes = list(fill = "black")))
+
+##### NEWFOUNDLAND DATA 
+
+
+# alter the dataframe so it is in appropriate format for NMDS
+# names_from: The column whose values will be used as column names
+# values_from: The column whose values will be used as cell values
+
+colnames(pacMerge)
+
+# Find a more elegant way to do this!!
+pacSpecies = pacMerge %>%
+  # Remember there will be duplicates because of the 5mm data! These need to be combined!!
+  group_by(sample, class, dataset, sumWaterVolume, yearStart, myLabel) %>% summarize(abund = sum(abund)) %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) # replace NAs with 0
+
+
+# Do NMDS but only include species data
+pacNMDS = metaMDS(sqrt(pacSpecies[,c(6:50)]), distance = "bray", autotransform=FALSE)
+
+# Get NMDS coordinates from plot
+pacCoords = as.data.frame(scores(pacNMDS, display="sites"))
+# How stressed am I today
+pacStress = paste("2D Stress: ", round(pacNMDS$stress, digits=2))
+
+g3 = 
+  ggplot() + 
+  geom_point(data = pacCoords, aes(x=NMDS1, y=NMDS2, fill=as.factor(pacSpecies$dataset)),
+             pch = 21, size = 7, alpha = 0.85)+ # Use pch=21 to get black outline circles
+  # Change legend names
+  scale_fill_discrete(name = "Location")+
+  scale_shape_manual(values=c(21, 22, 23, 24, 25), name = "Location within bay")+ 
+  
+  annotate("text", x = max(pacCoords$NMDS1), y=max(pacCoords$NMDS2), label = pacStress, size=3.5, hjust=1)+
+  theme_bw()+
+  theme(axis.text = element_blank(),
+        #axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        #legend.position = "none",
+        panel.border=element_rect(color="black", size=1), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank(),
+        plot.margin=unit(c(0.1, 0.1, 0.1, 0.1),"cm"))+
+  # Need to override and show these as a new new shape otherwise it won't show up
+  guides(fill = guide_legend(override.aes = list(shape = 21)),
+         shape = guide_legend(override.aes = list(fill = "black")))
 
 
