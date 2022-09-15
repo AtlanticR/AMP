b8c5830effa1f386300871530f5b585e224ff7a0
Stephen Finnis <stephenfinnis7@gmail.com>
2022-09-15 11:18:23 -0700

    make nmds as part of function

--- Figures/nmds.R
+++ Figures/nmds.R
@@ -11,7 +11,6 @@ source("C:/Users/FINNISS/Desktop/AMPcode/DataProcessing/zooplanktonCounts.R")
 
 ################################################################################
 
-
 # Create function to make NMDS ordinations
 
 nmdsPrep = function(mergeData) {
@@ -36,8 +35,31 @@ nmdsPrep = function(mergeData) {
   # How stressed am I today
   ordStress = paste("2D Stress: ", round(ord$stress, digits=2))
   
+  # Get the number of facets there should be (either # of bays, or # of sampling months (Pacific))
+  numFacet = length(unique(marMerge$facetFactor))
+  # create array for pch symbols. e.g., if 4 factors will give: 21, 22, 23, 24
+  numPch = c(21:(20+numFacet))
+  
+  g1 =
+  ggplot() + 
+    geom_point(data = ordCoords, aes(x=NMDS1, y=NMDS2, pch = mergeData$facetFactor, fill = mergeData$facetFactor, group=mergeData$facetFactor), size = 5)+ # Use pch=21 to get black outline circles
+    # Note, for legends to be combined (instead of 1 legend for points, one for fill, the name must be the same!)
+    scale_fill_discrete(name = "Bay")+
+    scale_shape_manual(values=numPch, name = "Bay")+ 
+    annotate("text", x = max(ordCoords$NMDS1), y=max(ordCoords$NMDS2), label = ordStress, size=3.5, hjust=1)+
+    theme_bw()+
+    theme(axis.text = element_blank(),
+          #axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          #legend.position = "none",
+          panel.border=element_rect(color="black", size=1), 
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          plot.background = element_blank(),
+          plot.margin=unit(c(0.1, 0.1, 0.1, 0.1),"cm"))
+  
   # Test that ordination stress is created (i.e., nmds works)
-  return(ordStress)
+  return(g1)
   
 }
 
