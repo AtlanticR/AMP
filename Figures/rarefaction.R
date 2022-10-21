99c56b146eccbc7752712f8acfd3cd755ef39920
Stephen Finnis <stephenfinnis7@gmail.com>
2022-10-21 11:02:12 -0700

    add script for testing various rarefaction methods

---
+++ Figures/rarefaction.R
@@ -0,0 +1,13 @@
+#### Rarefaction Curves
+
+# Just experimenting with creating rarefaction curves and all the different possible outputs
+
+
+
+data(BCI)
+S <- specnumber(BCI) # observed number of species
+(raremax <- min(rowSums(BCI)))
+Srare <- rarefy(BCI, raremax)
+plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
+abline(0, 1)
+rarecurve(BCI, step = 20, sample = raremax, col = "blue", cex = 0.6)
