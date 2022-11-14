57725c3a354c241e9981463a0c5756afde1b793b
Stephen Finnis <stephenfinnis7@gmail.com>
2022-11-14 14:51:12 -0800

    add venn diagrams for gulf and pacific

--- Figures/vennDiagram.R
+++ Figures/vennDiagram.R
@@ -15,41 +15,110 @@ library("ggVennDiagram")
 install.packages("ggvenn")
 library("ggvenn")
 
-testMar = marMerge %>%
+####################################################################################
+# Maritimes
+
+marTaxa = marMerge %>%
   group_by(class, facetFactor) %>%
   summarize(countPerClass = sum(abund)) %>%
+  # Remove taxa that are not present
   filter(countPerClass > 0)
 
-argyle = testMar %>%
+# Split up the data by bay
+argyleVen =  marTaxa %>%
   filter(facetFactor == "Argyle")
-sober = testMar %>%
+soberVenn = marTaxa %>%
   filter(facetFactor == "Sober Island")
-country = testMar %>%
+countryVenn = marTaxa %>%
   filter(facetFactor == "Country Harbour")
-whitehead = testMar %>%
+whiteheadVenn = marTaxa %>%
   filter(facetFactor == "Whitehead")
 
-argyleVec = as.vector(argyle$class)
-soberVec = as.vector(sober$class)
-countryVec = as.vector(country$class)
-whiteheadVec = as.vector(whitehead$class)
+# Turn it into a vector of taxa names (class) that are present
+argyleVec = as.vector(argyleVenn$class)
+soberVec = as.vector(soberVenn$class)
+countryVec = as.vector(countryVenn$class)
+whiteheadVec = as.vector(whiteheadVenn$class)
 
-
-x = list("Argyle" = argyleVec, "Sober Island" = soberVec, "Country Harbour" = countryVec, "Whitehead" = whiteheadVec)
+marBayVen = list("Argyle" = argyleVec, "Sober Island" = soberVec, "Country Harbour" = countryVec, "Whitehead" = whiteheadVec)
 
 # Option 1 (ugly, but more customizable)
-ggVennDiagram(x)
+ggVennDiagram(marBayVen)
 
 # Option 2 (prettier, but maybe less customizable?)
-ggvenn(x, set_name_size = 4.5)
+ggvenn(marBayVen, set_name_size = 4.5)
+
+
+####################################################################################
+# Gulf
+
+gulfTaxa = gulfMerge %>%
+  group_by(class, facetFactor) %>%
+  summarize(countPerClass = sum(abund)) %>%
+  # Remove taxa that are not present
+  filter(countPerClass > 0)
+
+# Split up the data by bay
+stPetersVen = gulfTaxa %>%
+  filter(facetFactor == "St. Peters")
+malpequeVen = gulfTaxa %>%
+  filter(facetFactor == "Malpeque")
+cocagneVen = gulfTaxa %>%
+  filter(facetFactor == "Cocagne")
+
+# Turn it into a vector of taxa names (class) that are present
+stPetersVec = as.vector(stPetersVen$class)
+malpequeVec = as.vector(malpequeVen$class)
+cocagneVec = as.vector(cocagneVen$class)
+
+gulfBayVen = list("St. Peters" = argyleVec, "Malpeque" = soberVec, "Cocagne" = cocagneVec)
+
+# Option 1 (ugly, but more customizable)
+ggVennDiagram(gulfBayVen)
+
+# Option 2 (prettier, but maybe less customizable?)
+ggvenn(gulfBayVen, set_name_size = 4.5)
+
+
+####################################################################################
+# Pacific
+
+pacTaxa = pacMerge %>%
+  group_by(class, facetFactor) %>%
+  summarize(countPerClass = sum(abund)) %>%
+  # Remove taxa that are not present
+  filter(countPerClass > 0)
+
+# Split up the data by bay
+aug20Ven = pacTaxa %>%
+  filter(facetFactor == "August 2020")
+mar21Ven = pacTaxa %>%
+  filter(facetFactor == "March 2021")
+jun21Ven = pacTaxa %>%
+  filter(facetFactor == "June 2021")
+sept21Ven = pacTaxa %>%
+  filter(facetFactor == "September 2021")
+
+
+# Turn it into a vector of taxa names (class) that are present
+aug20Vec = as.vector(aug20Ven$class)
+mar21Vec = as.vector(mar21Ven$class)
+jun21Vec = as.vector(jun21Ven$class)
+sept21Vec = as.vector(sept21Ven$class)
+
+pacVen = list("August 2020" = aug20Vec, "March 2021" = mar21Vec, "June 2021" = jun21Vec, "September 2021" = sept21Vec)
+
+# Option 1 (ugly, but more customizable)
+ggVennDiagram(pacVen)
+
+# Option 2 (prettier, but maybe less customizable?)
+ggvenn(pacVen, set_name_size = 4.5)
 
 
 
 
 
-
-
-###### 
+################## 
 # Rel abundance by region
 
 marTest = marMerge %>%
