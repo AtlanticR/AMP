1a61402833f0d48f32bad31f003cd36d8d26d631
Stephen Finnis <stephenfinnis7@gmail.com>
2022-11-23 10:29:51 -0800

    write the code!

--- DataProcessing/bayBreakdown.R
+++ DataProcessing/bayBreakdown.R
@@ -2,8 +2,7 @@
 ## DATA BY BAYS
 
 # I want to create dataframes with all the data for each bay
-# It will take the dataframes created at the end of zooplanktonCounts.R and break
-# them up by bay
+# It will take the dataframes created at the end of zooplanktonCounts.R and break them up by bay
 
 # This therefore contains the following info:
 # sampleCode, waterVolume, tideRange, yearStart, facilityName, myLabel, tidePhase, flowcamCode
@@ -11,11 +10,49 @@
 
 # It will also adjust the format of the data: tows as rows, species as columns, counts as cells (with the metadata also as columns)
 
-# Created by Stephen Finnis 2022
-
 ################################################################################
 
-# read in all required R packages
+# Read in the processed zooplankton counts
+# This will provide the dataframes with counts for each REGION (it's not broken up by bay yet)
 source("DataProcessing/zooplanktonCounts.R") 
 
 ################################################################################
+
+## Create a function to alter the data format
+breakupBayBay = function(regionData, bayName) {
+  # Change data with tows as rows, species as columns, counts as cells (with the metadata also as columns)
+  regionData %>%
+    pivot_wider(names_from = class, values_from = abund) %>%
+    mutate_all(~replace(., is.na(.), 0)) %>%
+    subset(facetFactor == bayName)
+}
+
+################################################################################
+## Now run the function for each bay
+# There is probably a more elegant way to do this so it's not repeated
+# But that is a task for another day!
+
+## MARITIMES
+argyle = breakupBayBay(marMerge, "Argyle")
+country = breakupBayBay(marMerge, "Country Harbour")
+sober = breakupBayBay(marMerge, "Sober Island")
+whitehead = breakupBayBay(marMerge, "Whitehead")
+
+
+## GULF
+cocagne = breakupBayBay(gulfMerge, "Cocagne")
+malpeque = breakupBayBay(gulfMerge, "Malpeque")
+stPeters = breakupBayBay(gulfMerge, "St. Peters")
+
+## PACIFIC
+# I have to be very careful with these names since there are similar variable names
+# created in zooplanktonCounts.R
+# Here, I will use the full year in the name
+pacAug2020 = breakupBayBay(pacMerge, "August 2020")
+pacMar2021 = breakupBayBay(pacMerge, "March 2021")
+pacJun2021 = breakupBayBay(pacMerge, "June 2021")
+pacSept2021 = breakupBayBay(pacMerge, "September 2021")
+
+## NEWFOUNDLAND
+# There's only one bay and time period (so far) but it still needs to have the layout adjusted
+seArm2020 = breakupBayBay(nlMerge, "Southeast Arm 2020")
