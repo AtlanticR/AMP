c4e8b8d8a34e90f36f77ec3e59e1e650616f9f51
Stephen Finnis <stephenfinnis7@gmail.com>
2022-12-02 08:39:28 -0800

    new script for study area maps with polygon coastline

---
+++ Figures/nonBasemapStudyAreaMaps.R
@@ -0,0 +1,15 @@
+################################################################################
+### STUDY AREA MAPS WITH POLYGONS INSTEAD OF BASEMAPS
+
+
+################################################################################
+## Read in relevant data processing code
+
+# Get the processed metadata files
+# These contain the sampling location, tow type, etc.
+# Will return data frames with metadata info for each AMP region
+source("DataProcessing/metadataProcessing.R")
+
+
+
+
