250f7096fb08ae55bc4e82bfbb98c6caa6f42612
Stephen Finnis <stephenfinnis7@gmail.com>
2022-12-02 08:27:46 -0800

    add script to get counts of copepods for UNB student

---
+++ DataProcessing/copepodCount.R
@@ -0,0 +1,37 @@
+###### COPEPODS
+# Get the counts for copepods for Anais
+
+source("DataProcessing/zooplanktonCounts.R")
+
+taxaCountsBay = rbind(gulfMerge, marMerge, nlMerge, pacMerge) %>%
+  group_by(class, dataset, facetFactor) %>%
+  summarize(countPerClass = sum(count)) %>%
+  filter(countPerClass > 0)%>%
+  filter(class == "Acartia spp." | 
+           class == "Calanoida (unid)"|
+      class == "Calanus spp." | 
+      class == "Centropages spp." | 
+      class == "Chiridus spp." | 
+      class == "Copepoda nauplii" | 
+      class == "Corycaeidae" |
+  class == "Cyclopoida" | 
+  class == "Eurytemora spp." | 
+  class == "Euterpina acutifrons" |
+  class == "Harpacticoida" |
+  class == "Heterorhabdus spp." |
+  class == "Labidocera spp." |
+  class == "Metridia spp." |
+  class == "Microcalanus spp." |
+  class == "Microsetella spp." |
+  class == "Monstrillidae" |
+  class == "Oithona spp." |
+  class == "Oncaeidae" |
+  class == "Paracalanus spp." |
+  class == "Pseudocalanus spp." |
+  class == "Pseudodiaptomus spp." |
+  class == "Temora spp." |
+  class == "Temoridae spp." |
+  class == "Tortanus spp.")
+  
+write.csv(taxaCountsBay, "copepodsByBay.csv")
+  
