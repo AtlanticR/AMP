2729feb31e7545603827efc8550db5df5f61623c
Stephen Finnis <stephenfinnis7@gmail.com>
2022-12-05 15:14:44 -0800

    read in shapefiles for each region

--- Figures/studyAreaPolygons.R
+++ Figures/studyAreaPolygons.R
@@ -1,36 +1,123 @@
 # Clipping land to a bounding box!
 # I definitely deleted a bunch of packages. But here's 
 
-test = readOGR("C:/Users/FINNISS/Desktop/FWApoly/FWApoly_reduced.shp")
 
-plot(test)
+source("DataProcessing/rPackages.R")
 
-fortPoly = fortify(test)
 
+lemmensCoastline = tidy(readOGR("C:/Users/FINNISS/Desktop/FWApoly/FWApoly_reduced.shp"))
+cocagneCoastline = tidy(readOGR("C:/Users/FINNISS/Desktop/FWApoly/cocagneCoastline.shp"))
+
+argyleCoastline = spTransform(readOGR("C:/Users/FINNISS/Desktop/FWApoly/argyleCoastline.shp"), crs = 4236)
+countryCoastline = tidy(readOGR("C:/Users/FINNISS/Desktop/FWApoly/countryCoastline.shp"))
+whiteheadCoastline = tidy(readOGR("C:/Users/FINNISS/Desktop/FWApoly/whiteheadCoastline.shp"))
+
+
+
+argCost = fortify(argyleCoastline)
+
+# Test what it looks like
+plot(argyleCoastline)
+
+# Pacific
 ggplot()+
-  geom_polygon(fortPoly, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
-  #coord_sf(crs = 4326)+
-  #geom_sf()+
-  #coord_map(xlim = c(-125.9472, -125.82), ylim = c(49.15, 49.24))+
+  geom_polygon(lemmensCoastline, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
+  geom_point(pacMeta, mapping = aes(x = longitude, y = latitude), pch = 21, col = "black", fill = "#C77CFF", size = 7, alpha = 0.6)+
   # Use this instead of coord_map to get the scalebar thing to work. 
   # annotation_scale needs the crs to be set here too
   coord_sf(xlim = c(-125.9472, -125.82), ylim = c(49.15, 49.24), crs = 4236)+
-  #geom_spatial_point(aes(x = 49.1171, y = -125.95), crs = 4326)+
+  annotation_scale(location = "br", text_cex = 1)+
   theme_bw()+
-  theme(panel.grid = element_blank(),
-        axis.text = element_text(size = 14))+
-  #coord_sf(crs = 4326)+
+  theme(
+        axis.text = element_text(size = 14),
+        axis.title = element_blank(),
+        panel.grid = element_blank())
   
-  #ggsn::scalebar(fortPoly, dist_unit = "km", transform = T, model = "WSG84")
+# Argyle                   
+ggplot()+
+  geom_polygon(argCost, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
+  geom_point(marMeta %>% filter(facilityName == "Argyle"), mapping = aes(x = longitude, y = latitude), pch = 21, col = "black", fill = "#C77CFF", size = 7, alpha = 0.6)+
+  # Use this instead of coord_map to get the scalebar thing to work. 
+  # annotation_scale needs the crs to be set here too
+  #coord_sf(xlim = c(-125.9472, -125.82), ylim = c(49.15, 49.24), crs = 4236)+
+  annotation_scale(location = "br", text_cex = 1)+
+  theme_bw()+
+  theme(
+    axis.text = element_text(size = 14),
+    axis.title = element_blank(),
+    panel.grid = element_blank())
+
+
+#########################
+
+install.packages("rnaturalearth")
+install.packages("rnaturalearthdata")
+devtools::install_github("ropensci/rnaturalearthhires")
+
+library("rnaturalearth")
+library("rnaturalearthdata")
+library("rnaturalearthhires")
+
+
+canada_map <- ne_states(country = "canada", returnclass = "sf")
+plot(canada_map)
+
+# Define the Lambert Conformal Conic projection
+# Taken from here: https://ahurford.github.io/quant-guide-all-courses/making-maps.html
+can.lcc = "+proj=lcc +lat_1=49 +lat_2=77 +lon_0=-91.52 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
+
+ggplot()+
+  geom_sf(data = canada_map, colour = "black", fill = "grey92")+
+  theme_bw()+
+  #theme(panel.background = element_rect(fill = "aliceblue"))+
+  coord_sf(crs = can.lcc)
+
+
+map_nl <- canada_map[canada_map$name == "Newfoundland and Labrador",]
+
+
+canada_again = ne_countries(country = "canada", returnclass = "sf", scale = "medium")
+
+
+ggplot()+
+  geom_sf(data = canada_again, colour = "black", fill = "grey92")+
+  theme_bw()+
+  coord_sf(crs = can.lcc)
+
+
+
+ggplot()+
+  geom_polygon(argCoastDf, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
+  theme_bw()
+
+ggplot()+
+  geom_polygon(counCoastDf, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
+  theme_bw()
+
+ggplot()+
+  geom_polygon(whitCoastDf, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
+  theme_bw()
+
+
+
+
+regions = readOGR("C:/Users/FINNISS/Desktop/dfoRegions.shp")
+d = fortify(regions, region = regions$data$region_EN)
+
+
+atlColour = c("Gulf" = "#F8766D", 
+              "Maritimes" = "#7CAE00", 
+              "Newfoundland" = "#00BFC4")
+# Pacific Ocean 
+# Note this is the colour when only Pacific OCEAN data is displayed
+# When Pacific (region) data is broken down by field season (below), 4 colours will be set
+pacColourOne = c("Pacific" = "#C77CFF")
+
+
+
+ggplot()+
+  geom_polygon(d, mapping = aes(x = long, y = lat, group=group, fill = id), col = "black")+
+  theme_bw()+
+  scale_fill_manual(values = c("#00BFC4", "#C77CFF", "grey90", "gray90", "gray90", "gray90", "#7CAE00", "#F8766D"))
   
-  ggspatial::annotation_north_arrow(
-    location = "br", which_north = "true",
-    #pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
-    height = unit(2, "cm"),
-    width = unit(2, "cm"),
-    style = ggspatial::north_arrow_nautical(
-      fill = c("grey40", "white"),
-      line_col = "grey20"))+
-  annotation_scale(location = "br", text_cex = 1)
-                   
 
