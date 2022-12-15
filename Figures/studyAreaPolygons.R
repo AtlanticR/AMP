ff5c847014e9a8d3d012326b55b93e88f4ebd53e
Stephen Finnis <stephenfinnis7@gmail.com>
2022-12-15 15:59:45 -0800

    get UTM coordinates for sites

--- Figures/studyAreaPolygons.R
+++ Figures/studyAreaPolygons.R
@@ -36,6 +36,62 @@ peiLeases = fortify(sp::spTransform(readOGR("C:/Users/FINNISS/Desktop/AMPDataFil
 
 
 ################################################################################
+### Converting station coordinates
+
+## Maritimes
+
+# Maritimes sites that were points (punctual stations)
+marPunctualWGS = st_as_sf(marMeta %>% filter(facilityName == "Country Harbour" | facilityName == "WhiteHead"),
+                          coords = c("longitude", "latitude"), crs = 4326)
+marPunctualUTM = st_transform(marPunctualWGS, CRS("+proj=utm +zone=20 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+# Maritimes sites that were transects
+# This is a MESS. I am 100% sure there is a better way to do this but I am in a rush!!
+# It is horrible to deal with actual line sf objects in R. Instead, I am converting coordinates to UTM (as points) and using geom_segment to map them
+# Get the coordinate positions for the beginning of the transect and convert to UTM for plotting 
+marTransectWGS = st_as_sf(marMeta %>% filter(facilityName == "Sober Island Oyster" | facilityName == "Argyle"),
+                          coords = c("longitude", "latitude"), crs = 4326)
+marTransectUTM = st_transform(marTransectWGS, CRS("+proj=utm +zone=20 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+# Get the coordinate positions for the end of the transect and convert to UTM for plotting 
+marTransectWGS.End = st_as_sf(marMeta %>% filter(facilityName == "Sober Island Oyster" | facilityName == "Argyle"),
+                          coords = c("longitudeEnd", "latitudeEnd"), crs = 4326)
+marTransectUTM.End = st_transform(marTransectWGS.End, CRS("+proj=utm +zone=20 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+# Those commands DID convert the coordinates! But now they are the final column called "Geometry" and both coordinates are in the same cell
+# I want to separate out into new columns
+# Make new columns (lon and lat) with their respective UTM coordinates (these are the beginning of the transect)
+marTransectUTM = marTransectUTM %>%
+  dplyr::mutate(lon = sf::st_coordinates(.)[,1],
+              lat = sf::st_coordinates(.)[,2])
+# Make new columns (lonEnd and latEnd) with their respective UTM coordinates (end of the transect)
+marTransectUTM.End = marTransectUTM.End %>%
+  dplyr::mutate(lonEnd = sf::st_coordinates(.)[,1],
+                latEnd = sf::st_coordinates(.)[,2])
+
+# Add the end ones to the original data frame
+marTransectUTM$lonEnd = marTransectUTM.End$lonEnd
+marTransectUTM$latEnd = marTransectUTM.End$latEnd
+
+# Remove the .End dataframe because it's probably taking up too much space
+rm(marTransectUTM.End)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
 ## PACIFIC 
 
 ggPacMap = ggplot()+
@@ -63,8 +119,7 @@ ggPacMap = ggplot()+
 ggArgMap = ggplot()+
   geom_polygon(argyleCoastline, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
   geom_polygon(nsLeases, mapping = aes(x = long, y= lat, group = group), fill = "pink", col = "red")+
-  #geom_point(marMeta %>% filter(facilityName == "Argyle"), mapping = aes(x = longitude, y = latitude), pch = 21, col = "black", fill = "#C77CFF", size = 7, alpha = 0.6)+
-  #geom_segment(marMeta %>% filter(facilityName == "Argyle"), mapping = aes(x = longitude, xend = longitudeEnd, y = latitude, yend = latitudeEnd), col = "darkgreen", alpha = 0.6, linewidth = 4, lineend = "round")+
+  geom_segment(marTransectUTM, mapping = aes(x = lon, xend = lonEnd, y = lat, yend = latEnd), col = "darkgreen", alpha = 0.6, linewidth = 3, lineend = "round")+
     # Use this instead of coord_map to get the scalebar thing to work. 
   # annotation_scale needs the crs to be set here too
   coord_sf(xlim = c(259213, 267918), ylim = c(4846335, 4855031), crs = 32620)+
@@ -84,7 +139,7 @@ ggArgMap = ggplot()+
 ggChMap = ggplot()+
   geom_polygon(countryCoastline, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
   geom_polygon(nsLeases, mapping = aes(x = long, y= lat, group = group), fill = "pink", col = "red")+
-  #geom_point(marMeta %>% filter(facilityName == "Country Harbour"), mapping = aes(x = longitude, y = latitude), pch = 21, col = "black", fill = "#C77CFF", size = 7, alpha = 0.6)+
+  geom_sf(data = marPunctualUTM, pch = 21, col = "black", fill = "green3", size = 7, alpha = 0.6)+
   # Use this instead of coord_map to get the scalebar thing to work. 
   # annotation_scale needs the crs to be set here too
   coord_sf(xlim = c(596383, 611074), ylim = c(4996642, 5011378), crs = 32620)+
@@ -103,8 +158,7 @@ ggChMap = ggplot()+
 ggWhMap = ggplot()+
   geom_polygon(whiteheadCoastline, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
   geom_polygon(nsLeases, mapping = aes(x = long, y= lat, group = group), fill = "pink", col = "red")+
-  #geom_point(marMeta %>% filter(facilityName == "WhiteHead"), mapping = aes(x = longitude, y = latitude), pch = 21, col = "black", fill = "#C77CFF", size = 7, alpha = 0.6)+
-  # Use this instead of coord_map to get the scalebar thing to work. 
+  geom_sf(data = marPunctualUTM, pch = 21, col = "black", fill = "mediumspringgreen", size = 7, alpha = 0.6)+  # Use this instead of coord_map to get the scalebar thing to work. 
   # annotation_scale needs the crs to be set here too
   coord_sf(xlim = c(641669, 647881), ylim = c(5013443, 5019630), crs = 32620)+
   annotation_scale(location = "br", text_cex = 1)+
@@ -122,8 +176,7 @@ ggWhMap = ggplot()+
 ggSobMap = ggplot()+
   geom_polygon(soberCoastline, mapping = aes(x = long, y = lat, group=group), fill = "gray92", col = "black")+
   geom_polygon(nsLeases, mapping = aes(x = long, y= lat, group = group), fill = "pink", col = "red")+
-  #geom_point(marMeta %>% filter(facilityName == "Sober Island Oyster"), mapping = aes(x = longitude, y = latitude), pch = 21, col = "black", fill = "darkgreen", size = 7, alpha = 0.6)+
-  #geom_segment(marMeta %>% filter(facilityName == "Sober Island Oyster"), mapping = aes(x = longitude, xend = longitudeEnd, y = latitude, yend = latitudeEnd), col = "mediumspringgreen", alpha = 0.6, linewidth = 4, lineend = "round")+
+  geom_segment(marTransectUTM, mapping = aes(x = lon, xend = lonEnd, y = lat, yend = latEnd), col = "mediumspringgreen", alpha = 0.6, linewidth = 3, lineend = "round")+
   # Use this instead of coord_map to get the scalebar thing to work. 
   # annotation_scale needs the crs to be set here too
   coord_sf(xlim = c(541154, 542825), ylim = c(4964661, 4966410), crs = 32620)+
@@ -264,14 +317,18 @@ ggplot()+
 can.lccTake2 = "+proj=lcc +lat_1=50 +lat_2=70 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
 
 
+mean(c(2173445, 3097367))
+911226-10000
 
 canMapTake2 = 
   ggplot()+
   geom_sf(data = canada_map, colour = "black", fill = "grey92")+
   theme_bw()+
   ggtitle("(A) Canada")+
-  geom_rect(aes(xmin = 2173445, xmax = 3097367, ymin = 911226, ymax = 2161814), col = "red", fill = NA)+
-  geom_rect(aes(xmin = -2243138, xmax = -1907966, ymin = 1323684, ymax = 1843861), col = "red", fill = NA)+
+  geom_rect(aes(xmin = -2243138, xmax = -1907966, ymin = 1323684, ymax = 1843861), col = "red", fill = NA)+ # Pacific inset
+  geom_text(data = canada_map, aes(x = mean(c(-2243138, -1907966)), y=1323684-200000), label="B", col = "red", size = 6)+ # label for Pacific
+  geom_rect(aes(xmin = 2173445, xmax = 3097367, ymin = 911226, ymax = 2161814), col = "red", fill = NA)+ # Atlantic inset
+  geom_text(data = canada_map, aes(x = mean(c(2173445, 3097367)), y=907226-200000), label="C", col = "red", size = 6)+ # label for Atlantic
   #theme(panel.background = element_rect(fill = "aliceblue"))+
   coord_sf(crs = can.lccTake2)+
   theme(
