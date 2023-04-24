414b539fcc592fedb0e905524ac68ef06c246b75
Stephen Finnis <stephenfinnis7@gmail.com>
2023-04-24 14:12:28 -0700

    conduct T-Tests to evaluate tide effect

--- Figures/tideTest.R
+++ Figures/tideTest.R
@@ -15,5 +15,179 @@ cohensD(group1, group2)
 install.packages("effsize")
 library("effsize")
 
+install.packages("rempsyc")
+library("rempsyc")
+
 cohen.d(group1, group2)
+
+###############################################################################
+## Which tides am I testing between
+
+# Pacific:
+# August 2020: high vs low at all stations
+
+aug20HT.outer = pacAug2020 %>%
+  filter(tidePhase == "High" & myLabel == "Outer") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+aug20LT.outer = pacAug2020 %>%
+  filter(tidePhase == "Low" & myLabel == "Outer") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(aug20HT.outer, aug20LT.outer, var.equal=T, alternative = "two.sided")
+
+
+
+##
+aug20HT.mid = pacAug2020 %>%
+  filter(tidePhase == "High" & myLabel == "Mid") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+aug20LT.mid = pacAug2020 %>%
+  filter(tidePhase == "Low" & myLabel == "Mid") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(aug20HT.mid, aug20LT.mid, var.equal=T, alternative = "two.sided")
+
+
+####
+aug20HT.inner = pacAug2020 %>%
+  filter(tidePhase == "High" & myLabel == "Inner") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+aug20LT.inner = pacAug2020 %>%
+  filter(tidePhase == "Low" & myLabel == "Inner") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(aug20HT.inner, aug20LT.inner, var.equal=T, alternative = "two.sided")
+
+# June 2021: high vs low at Mid stations
+jun21HT.mid = pacJun2021 %>%
+  filter(tidePhase == "High" & myLabel == "Mid") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+jun21LT.mid = pacJun2021 %>%
+  filter(tidePhase == "Low" & myLabel == "Mid") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(jun21HT.mid, jun21LT.mid, var.equal=T, alternative = "two.sided")
+
+
+
+# Maritimes: 
+# Sober Island: Outer station (high vs low)
+
+soberHT.outer = sober %>%
+  filter(tidePhase == "High" & myLabel == "Outer") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+soberLT.outer = sober %>%
+  filter(tidePhase == "Low" & myLabel == "Outer") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(soberHT.outer, soberLT.outer, var.equal=T, alternative = "two.sided")
+
+
+
+# Gulf: 
+# I think all stations, but note Idk because sample size is different?
+
+# Outer 
+stpHT.outer = stPeters %>%
+  filter(tidePhase == "High" & myLabel == "Outer") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+stpLT.outer = stPeters %>%
+  filter(tidePhase == "Low" & myLabel == "Outer") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(stpHT.outer, stpLT.outer, var.equal=T, alternative = "two.sided")
+
+## Mid
+stpHT.mid = stPeters %>%
+  filter(tidePhase == "High" & myLabel == "Mid") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+stpLT.mid = stPeters %>%
+  filter(tidePhase == "Low" & myLabel == "Mid") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(stpHT.mid, stpLT.mid, var.equal=T, alternative = "two.sided")
+
+### Inner
+stpHT.inner = stPeters %>%
+  filter(tidePhase == "High" & myLabel == "Inner") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+stpLT.inner = stPeters %>%
+  filter(tidePhase == "Low" & myLabel == "Inner") %>%
+  select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(stpHT.inner, stpLT.inner, var.equal=T, alternative = "two.sided")
+
+# Newfoundland: Outer High vs Low
+
+nlHT.outer = nl %>%
+  filter(monthStart == 10 & yearStart == 2021) %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) %>%  # replace NAs with 0
+  filter(tidePhase == "High" & myLabel == "Outer") %>%
+  #select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+nlLT.outer = nl %>%
+  filter(monthStart == 10 & yearStart == 2021) %>%
+  pivot_wider(names_from = class, values_from = abund) %>%
+  mutate_all(~replace(., is.na(.), 0)) %>%  # replace NAs with 0
+  filter(tidePhase == "Low" & myLabel == "Outer") %>%
+  #select(-c("waterVolAnalyzed")) %>%
+  select(starts_with("Acartia"):last_col()) %>%
+  specnumber()
+
+t.test(nlHT.outer, nlLT.outer, var.equal=T, alternative = "two.sided")
+
+
+install.packages("SpadeR")
+library("SpadeR")
+
+
+
+
+
+
+
+
 
