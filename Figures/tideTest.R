bab9e4290eb679027af876e6ed3391da55058482
Stephen Finnis <stephenfinnis7@gmail.com>
2023-05-02 11:32:43 -0700

    add final comments

--- Figures/tideTest.R
+++ Figures/tideTest.R
@@ -33,22 +33,27 @@ source("DataProcessing/bayBreakdown.R")
 options(scipen = 999)
 
 ###############################################################################
-## Which tides am I testing between
+## Run the tests
 
-
-
+# Create a function to run the t-tests and organize all the info in a nice format
+# Function passes in the species data frame for each site, the station being tested, and
+# the name of the site (that will be included in the table)
 tTestFun = function(specDF, station, site) {
 
-    dfHT = specDF %>%
+    # Reorganize the data frame for HIGH TIDE samples
+  dfHT = specDF %>%
     filter(tidePhase == "High" & myLabel == station) %>%
+    # Remove the waterVolAnalyzed column (it's required for rarefaction)
     select(-c("waterVolAnalyzed")) %>%
+    # Only look at data from Acartia (first alphabetically) to the end of specDF
     select(starts_with("Acartia"):last_col()) %>%
-    mutate(abund = rowSums(.),
-           rich = specnumber(.),
-           shan = exp(diversity(., index = "shannon")),
-           sim = diversity(., index = "invsimpson"))
+    mutate(abund = rowSums(.), # abundance
+           rich = specnumber(.), # richness
+           shan = exp(diversity(., index = "shannon")), # shannon DIVERSITY (exponential of shannon index)
+           sim = diversity(., index = "invsimpson")) # simpson DIVERSITY
     
-   dfLT = specDF %>%
+   # Reorganize the data for LOW TIDE samples  
+  dfLT = specDF %>%
      filter(tidePhase == "Low" & myLabel == station) %>%
      select(-c("waterVolAnalyzed")) %>%
      select(starts_with("Acartia"):last_col()) %>%
@@ -56,13 +61,19 @@ tTestFun = function(specDF, station, site) {
             rich = specnumber(.),
             shan = exp(diversity(., index = "shannon")),
             sim = diversity(., index = "invsimpson"))
-    
+  
+   # Run the two-tailed t-tests
+   # setting var.equal = T means the Welch test is not done (recommended for small sample sizes, see paper in Background section)
+   # Use the tidy() function to get results in a better format (tibble)
    tTestResults = bind_rows(
      tidy(t.test(dfHT$abund, dfLT$abund, var.equal=T, alternative = "two.sided")),
      tidy(t.test(dfHT$rich, dfLT$rich, var.equal=T, alternative = "two.sided")),
      tidy(t.test(dfHT$shan, dfLT$shan, var.equal=T, alternative = "two.sided")),
      tidy(t.test(dfHT$sim, dfLT$sim, var.equal=T, alternative = "two.sided"))
    ) %>%
+     
+     # Reorganize created dataframe to get it in the format I want (e.g., select certain columns, round info, etc)
+     # Note rows go: abundance, richness, shannon, simpson. I'll do this by hand later (it's easier)
      mutate(ci95 = paste0("[", round(conf.low,2), ", ", round(conf.high,2), "]")) %>%
      select(estimate1, estimate2, parameter, statistic, ci95, p.value) %>%
      mutate(stationName = station, .before = estimate1) %>%
@@ -70,12 +81,11 @@ tTestFun = function(specDF, station, site) {
      mutate(across(c(estimate1, estimate2, statistic), round, 2)) %>%
      mutate(across(c(p.value), round, 3)) 
      
-  
 }
 
 
-
-x= rbind(
+# Run the t-tests for each site. Store the info in a data frame
+tTestResults= rbind(
   tTestFun(pacAug2020, "Outer", "Lemmens Aug 2020"),
   tTestFun(pacAug2020, "Mid", "Lemmens Aug 2020"),
   tTestFun(pacAug2020, "Inner", "Lemmens Aug 2020"),
@@ -87,7 +97,8 @@ x= rbind(
   tTestFun(seArm2021, "Outer", "Southeast Arm Oct 2021")
   )
 
-
+# Write data to a csv
+# write.csv(tTestResults, "tTestResults.csv")
 
 
 
