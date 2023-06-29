9cccc20a91f295258999a70483d3188d642f5e01
Stephen Finnis <stephenfinnis7@gmail.com>
2023-06-29 16:11:42 -0700

    add charts that compare abundances and total counts per subsample

--- QuantitativeAssessment/barChartsQA.R
+++ QuantitativeAssessment/barChartsQA.R
@@ -46,10 +46,12 @@ barChart = function(qaData, fcData){
   # Add this these new classes as a column in the original data frame for plotting
   datPlotFC = datRankedFC %>%
     left_join(fcData) %>% 
-    group_by(classRanks, FlowCamID) %>%
+    group_by(classRanks, FlowCamID, type) %>%
     # If you don't recompute counts, the "Other" class will have a bunch of black lines
     # if you set the outline colour to black in geom_bar
-    summarise(sumCount = sum(count))
+    summarise(sumCount = sum(count),
+              abund = sum(abund),
+              countTot = sum(countTot))
   
   # Create stacked bar chart
   stackedBarFC = 
@@ -84,6 +86,10 @@ barChart = function(qaData, fcData){
     )
   
   
+
+  
+  
+  
   ### Quantitative Assessment data
   
   # Find the __ most abundant taxa. Label all others as "Other"
@@ -100,16 +106,21 @@ barChart = function(qaData, fcData){
   
   # UPDATE: I actually want the colour scheme to be the same as it is for the FlowCam data 
   # Add this these new classes as a column in the original data frame for plotting
-  datPlotQA = datRankedFC %>% # replace with "datRankedQA" if using unique colour scheme
+  datPlotQA = datRankedQA %>% # replace with "datRankedQA" if using unique colour scheme
     left_join(qaData) %>% 
-    group_by(classRanks, FlowCamID) %>%
+    group_by(classRanks, FlowCamID, type) %>%
     # If you don't recompute counts, the "Other" class will have a bunch of black lines
     # if you set the outline colour to black in geom_bar
-    summarise(sumCount = sum(count)) %>%
+    summarise(sumCount = sum(count),
+              abund = sum(abund),
+              countTot = sum(countTot)) %>%
     # Remove these last 2 lines if QA data should have its own unique colour scheme
     # This will join the FlowCam colour scheme to the QA data. but will result in NAs because sometimes QA doesn't have the same classes
     # Change these NAs to zero. Also the FlowCam ID will be NA so if that's the case, just set it to a random (I chose 30) flowcam ID instead
     mutate(sumCount = ifelse(is.na(sumCount), 0, sumCount),
+           abund = ifelse(is.na(abund), 0, abund),
+           countTot = ifelse(is.na(countTot), 0, countTot),
+           type = ifelse(is.na(type), "QA", type),
            FlowCamID = ifelse(is.na(FlowCamID), qaData$FlowCamID[30], FlowCamID)) 
   
   # Create stacked bar chart
@@ -143,10 +154,50 @@ barChart = function(qaData, fcData){
       #panel.grid.major.y = element_blank(),
     )
 
+  
+  
+  g1 = 
+    ggplot()+
+    geom_col(data = rbind(datPlotFC, datPlotQA), aes(x=type, y=abund, fill=classRanks), col = "black", linewidth = 0.05) + 
+    facet_grid(.~FlowCamID)+
+    scale_fill_brewer(palette = "Set3", name = "")+
+    scale_x_discrete(name = "Sample")+
+    theme_bw()+
+    theme(
+      axis.text.x = element_blank(),
+      # axis.text.x = element_text(angle = 90),
+      axis.ticks.x = element_blank())
+  
+  g2 = 
+    ggplot()+
+    geom_col(data = rbind(datPlotFC, datPlotQA), aes(x=type, y= countTot, fill=classRanks), col = "black", linewidth = 0.05) + 
+    facet_grid(.~FlowCamID)+
+    scale_fill_brewer(palette = "Set3", name = "")+
+    scale_x_discrete(name = "Sample")+
+    theme_bw()+
+    theme(
+      axis.text.x = element_blank(),
+      # axis.text.x = element_text(angle = 90),
+      axis.ticks.x = element_blank())
+  
+  # g2 = 
+  #   ggplot()+
+  #   geom_col(data = test3, aes(x=type, y=countTot, fill=newName)) + 
+  #   facet_grid(.~FlowCamID)
+  
+  
+  
+  ggarrange(g1, g2)
+  
+  
+  
 
   # Put everything together
   # ggarrange makes plots line up even when legends are different sizes
-  ggarrange(stackedBarFC, relAbundFC, stackedBarQA, relAbundQA, ncol = 2)
+  # ggarrange(stackedBarFC, relAbundFC, stackedBarQA, relAbundQA, ncol = 2)
+  #ggarrange(g1, g2)
+  
+  #return(datPlotQA)
 }
 
 
@@ -155,6 +206,33 @@ gulf20Charts = barChart(allQAData %>% subset(regionYear == "Gulf 2020"), flowCam
 pac21Charts = barChart(allQAData %>% subset(regionYear == "Pac 21"), flowCamData %>% subset(regionYear == "Pac 21"))
 nl20Charts = barChart(allQAData %>% subset(regionYear == "NL 2020"), flowCamData %>% subset(regionYear == "NL 2020"))
 nl21Charts = barChart(allQAData %>% subset(regionYear == "NL 2021"), flowCamData %>% subset(regionYear == "NL 2021"))
+
+################################################################################
+################################################################################
+# Now compare abundances in seawater
+
+
+test =  allQAData %>% subset(regionYear == "Gulf 2020")
+test2 = flowCamData %>% subset(regionYear == "Gulf 2020")
+
+
+test3 = rbind(test, test2)
+
+
+
+g1 = 
+ggplot()+
+  geom_col(data = test3, aes(x=type, y=abund, fill=newName)) + 
+  facet_grid(.~FlowCamID)
+
+g2 = 
+  ggplot()+
+  geom_col(data = test3, aes(x=type, y=countTot, fill=newName)) + 
+  facet_grid(.~FlowCamID)
+
+
+
+ggarrange(g1, g2)
 
 
 ################################################################################
@@ -177,15 +255,44 @@ sampleSummary = fcQaData %>%
   mutate(meanAbund = mean(relAbund),
          sdAbund = sd(relAbund)) %>%
   select(newName, regionYear, meanAbund, sdAbund, type) %>%
-  distinct()
+  distinct() 
+
+# I want to know, for each regionYear, which taxa are found: in both flowcam and microsopy, flowcam only, and microscopy only
+# It is very annoying to do lol
+sampleSummaryAdj = sampleSummary %>%
+  # First need to expand the list, so it has ALL comparisons 
+  # Because previously, only taxa that are present are recorded for each regionYear/type
+  # But if, e.g., Calanoida (unid) only present in FlowCam but not in microscopy, I need a new line of data for that too
+  ungroup() %>%
+  complete(newName, regionYear, type, fill = list(meanAbund =0, sdAbund = 0)) %>%
+
+  # Now do the comparisons.
+  group_by(newName, regionYear) %>%
+  mutate(presence = case_when(
+    all(meanAbund == 0) ~ "neither", # Not present in either
+    any(meanAbund >0) & all(meanAbund >0) ~ "Both", # present in both
+    meanAbund[type == "QA"] == 0 & meanAbund[type == "FC"] > 0 ~ "FC Only", # present in flowcam only
+    meanAbund[type == "QA"] > 0 & meanAbund[type == "FC"] == 0 ~ "MC Only" # present in microscopy only
+  )) 
+
+
 
 # Create a function to make bar chart comparisons for each region
 barChartComparison = function(relData, regionYear, plotName) {
-
-  ggplot(relData, aes(x = meanAbund, y = newName, fill = type, col = type))+
-    geom_errorbar(aes(xmin=meanAbund, xmax=meanAbund+sdAbund), width=.2,
-                  position=position_dodge(.8), col = "black")+
+  
+  # Don't include the data that are present in neither
+  # That would include taxa that are present in other region years, but not these ones
+  relData = relData %>%
+    filter(presence != "neither")
+  
+  # Make a plot that has 3 panels: present in both, present in flowcam only, present in microscopy only
+  ggplot(relData, aes(x = meanAbund, y = newName, fill = type))+
+    # geom_errorbar(aes(xmin=meanAbund, xmax=meanAbund+sdAbund), width=.2,
+    #               position=position_dodge(.8), col = "black")+
     geom_col(position = position_dodge2(preserve = "single"), width = 0.8)+
+    #facet_wrap(~presence, scales = "free_y", ncol = 1)+
+    # facet_grid seems better for getting bar width the same, but panel sizes to be different
+    facet_grid(presence~., scales = "free", space = "free", switch = "y")+ 
     xlab("Average relative abundance (%)")+
     ggtitle(plotName)+
     scale_fill_discrete(labels=c('FlowCam', 'Microscopy'))+
@@ -193,14 +300,18 @@ barChartComparison = function(relData, regionYear, plotName) {
     theme_bw()+
     theme(
       axis.title.y = element_blank(),
-      legend.title = element_blank())
+      axis.title.x = element_text(size = 12),
+      legend.text = element_text(size = 11),
+      legend.title = element_blank(),
+      strip.text.y = element_text(size = 10)
+      )
   
 }
   
 # Create the bar charts for each region separately
-barChartComparison(sampleSummary %>% filter(regionYear == "NL 2020"), "NL 2020", "Newfoundland 2020")
-barChartComparison(sampleSummary %>% filter(regionYear == "NL 2021"), "NL 2021", "Newfoundland 2021")
-barChartComparison(sampleSummary %>% filter(regionYear == "Pac 21"), "Pac 21", "Pacific 2021") 
-barChartComparison(sampleSummary %>% filter(regionYear == "Gulf 2020"), "Gulf 2020", "Gulf 2020") 
+barChartComparison(sampleSummaryAdj %>% filter(regionYear == "NL 2020"), "NL 2020", "Newfoundland 2020")
+barChartComparison(sampleSummaryAdj %>% filter(regionYear == "NL 2021"), "NL 2021", "Newfoundland 2021")
+barChartComparison(sampleSummaryAdj %>% filter(regionYear == "Pac 21"), "Pac 21", "Pacific 2021") 
+barChartComparison(sampleSummaryAdj %>% filter(regionYear == "Gulf 2020"), "Gulf 2020", "Gulf 2020") 
 
 
