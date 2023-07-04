8601e45663db1a1825f2c2ba8413129f7b2cedf2
Stephen Finnis <stephenfinnis7@gmail.com>
2023-07-04 14:00:01 -0700

    create new script for conducting t-tests for each taxa between QA/FC

---
+++ QuantitativeAssessment/tTest.R
@@ -0,0 +1,15 @@
+################################################################################
+################################################################################
+##### T-Tests
+
+# Conduct t-tests (?) between relative abundance of taxa from flowcam vs microscopy
+
+################################################################################
+
+# Read in the script that creates bar charts for the different comparisons
+# This has already created a dataframe of relative abundance for each sample
+source("QuantitativeAssessment/barChartsQA.R")
+
+
+
+
