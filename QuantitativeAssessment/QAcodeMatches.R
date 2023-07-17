184042b59cb221e3864f4b13bc0bd3d1c4dc451f
Stephen Finnis <stephenfinnis7@gmail.com>
2023-07-17 14:08:46 -0700

    delete spaces. test new PAT

--- QuantitativeAssessment/QAcodeMatches.R
+++ QuantitativeAssessment/QAcodeMatches.R
@@ -230,49 +230,51 @@ fcQaDf = rbind(allQAData, flowCamData)
 
 
 redistribute_abundances <- function(df, damaged_taxa, target_taxa){
-  
+
   # Filter the data frame for the damaged taxa
   df_damaged = df %>%
     filter(newName == damaged_taxa)
-  
+
   # Calculate the total abundance of the damaged taxa for each sample
   df_damaged_total = df_damaged %>%
     group_by(FlowCamID, type) %>%
     summarize(total_abund_damaged = sum(abund))
-  
+
   # Join the total abundance back to the original dataframe
   df = df %>%
     left_join(df_damaged_total, by = c("FlowCamID", "type"))
-  
+
   # Calculate the total abundance of the target taxa for each sample
   df_total <- df %>%
     filter(newName %in% target_taxa) %>%
     group_by(FlowCamID, type) %>%
     summarize(total_abund_target = sum(abund))
-  
+
   # Join the total abundance back to the original dataframe
   df = df %>%
     left_join(df_total, by = c("FlowCamID", "type"))
+
+  
   
   # Calculate the relative abundance of the target taxa within each sample
   for (taxon in target_taxa) {
     df = df %>%
       mutate(!!paste0("prop_", taxon) := if_else(newName == taxon, abund / total_abund_target, 0))
   }
-  
+
   # Calculate the redistributed abundances
   for (taxon in target_taxa) {
     df = df %>%
       mutate(abund = if_else(newName == taxon & !is.na(total_abund_damaged),
                              abund + get(paste0("prop_", taxon)) * total_abund_damaged, abund))
   }
-  
+
   # Remove the damaged taxa from the dataframe
   df = df %>% filter(newName != damaged_taxa)
-  
+
   # Drop the unnecessary columns
   df = df %>% select(-c(total_abund_damaged, total_abund_target, starts_with("prop_")))
-  
+
   return(df)
 }
 
@@ -302,9 +304,11 @@ test = redistribute_abundances(test, "Decapoda- CHECK", c("Decapoda- brachyura (
 
 # Redistribute Cyclopoida
 # NOTE: NEED TO MAKE A CHECK TO SEE WHAT HAPPENS IF NO CYLCOPOIDA CHILD
-test = redistribute_abundances(test, "Cyclopoida", c("Cyclopoida ci-ciii", "Corycaeidae", "Oithona spp."))
+# test = redistribute_abundances(test, "Cyclopoida", c("Cyclopoida ci-ciii", "Corycaeidae", "Oithona spp."))
 
-# Redistribute Calanoida 
+test = redistribute_abundances(test, "Cyclopoida", c("HI", "DOG"))
+
+# Redistribute Calanoida
 test = redistribute_abundances(test, "Calanoida (unid)", c("Acartia spp.", "Calanoida ci-ciii", "Calanus spp.", "Centropages spp.", "Chiridius spp.", "Eurytemora spp.",
                                "Metridia spp.", "Microcalanus spp.", "Paracalanus spp.", "Pseudocalanus spp.", "Pseudodiaptomus spp.", "Temora spp.", "Tortanus spp."))
 
@@ -322,8 +326,18 @@ test = test %>%
 
 
 fcQaDf = test
+# 
+# # Redistribute abundances of unidentified zooplankton between all other taxa present in the dataset
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# fcQaDf = df2
 
-# Redistribute abundances of unidentified zooplankton between all other taxa present in the dataset
 
 
 
@@ -332,5 +346,19 @@ fcQaDf = test
 
 
 
-fcQaDf = df2
+
+##### TESTING TESTING 
+target_species = "Acartia spp."
+
+
+exclude_species = c("Evadne spp.")
+
+
+desired_samples = fcQaData %>%
+  filter(newName == target_species) %>%
+  group_by(FlowCamID) %>%
+  filter(all(!(newName %in% exclude_species))) %>%
+  distinct(FlowCamID)
+  
+
 
