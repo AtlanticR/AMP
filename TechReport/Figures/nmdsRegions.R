c1a318f9ce9d95996ffe9dd90cd87e4ed58af4fd
Stephen Finnis <stephenfinnis7@gmail.com>
2023-08-10 10:02:07 -0700

    update directory

--- TechReport/Figures/nmdsRegions.R
+++ TechReport/Figures/nmdsRegions.R
@@ -15,9 +15,9 @@
 # This has all the plankton data with counts for each file
 # source("DataProcessing/zooplanktonCounts.R")
 
-source("DataProcessing/dividePlankton.R")
+source("TechReport/DataProcessing/dividePlankton.R")
 # This sets the colours schemes and symbology for bays, regions, etc
-source("Figures/colourPchSchemes.R")
+source("TechReport/Figures/colourPchSchemes.R")
 
 #################################################################################
 #################################################################################
