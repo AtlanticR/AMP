3a44ce565a74134a308162ec482b795aa82453ba
Stephen Finnis <stephenfinnis7@gmail.com>
2023-09-15 12:41:45 -0700

    write as excel file with each sheet as different grouping variable. calculate total counts

--- QuantitativeAssessment/dataProcessing/countsForLaval.R
+++ QuantitativeAssessment/dataProcessing/countsForLaval.R
@@ -467,8 +467,9 @@ allSites = rbind(gulfMerge, nlMerge, marMerge, pacMerge) %>%
 particleData = rbind(gulfMerge, nlMerge, marMerge, pacMerge) %>% 
   left_join(qaID, by = c("flowcamCode" = "FlowCamID")) %>%
   ungroup() %>%
-  select(sampleCode, class, newName, count, regionYear, selectForAnalysis) %>%
+  select(sampleCode, flowcamCode, class, newName, count, regionYear, selectForAnalysis) %>%
   filter(selectForAnalysis == "Yes") %>%
+  # Fix typos, etc.
   mutate(class = if_else(class == "0-250um", "0-250um Length", class),
          class = if_else(class == "Clumped Zooplanlton/Debris", "Clumped Zooplankton/Debris", class),
          class = if_else(class == "Clumped_zooplankton", "Clumped Zooplankton", class),
@@ -477,25 +478,32 @@ particleData = rbind(gulfMerge, nlMerge, marMerge, pacMerge) %>%
          class = if_else(newName != "Remove", "Zooplankton", class),
          class = if_else(is.na(class), "Zooplankton", class)) %>%
   filter(count != 0) %>%
-  group_by(sampleCode, class, regionYear) %>%
-  summarize(total_count = sum(count))
+  # Get total counts for each class within each sample
+  group_by(sampleCode, flowcamCode, class, regionYear) %>%
+  summarize(total_class_count = sum(count)) %>%
+  # Get total count of all particle types for entire sample
+  group_by(sampleCode) %>%
+  mutate(totalSample = sum(total_class_count)) %>%
   
+  # But note: the classes labelled 1-10 contain particles that were not actually counted
+  # Make these NAs. Add this as an extra column 
+  # (I could have deleted them, but it is easier to just include them for now!)
+  mutate(classCountNoNum = ifelse(class %in% as.character(0:10), NA, total_class_count))%>%
+  # Now recalculate the total counts without these NA values
+  group_by(sampleCode) %>%
+  mutate(totalSampleNoNum = sum(classCountNoNum, na.rm = T))
   
-  
+
+# Write the data to an excel file where every regionYear is a different sheet
+# First split the data based on regionYear 
+grouped_data = particleData %>%
+  split(.$regionYear)
+
+# Then write it out
+write_xlsx(grouped_data, "test.xlsx")
 
 
 
-
-
-
-# write.csv(allSites, "allSites.csv")
-
-# allSites = rbind(gulfMerge, nlMerge, marMerge, pacMerge) %>%
-#   
-#   #pivot_wider(names_from = newName, values_from = count) %>%
-#   mutate_all(~replace(., is.na(.), 0)) %>%
-#   group_by(facetFactor) %>%
-#   summarize(tot = sum(count))
 
 
 ################################################################################
